2e9e8424bfe7066f64f4dc4647289322f4c63119
Snehal Ahire <155544184+Snehal108@users.noreply.github.com>
2025-05-17 17:14:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Message sent! I’ll get back to you soon.");
+    this.reset();
+  });
+  
